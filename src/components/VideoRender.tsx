59689f6fff868318056294de3bf5e53a4c022691
Jerry Li <jerryliuoft@gmail.com>
2023-07-22 12:48:03 -0400

    Added popup when compile finishes

--- src/components/VideoRender.tsx
+++ src/components/VideoRender.tsx
@@ -45,7 +45,9 @@ const VideoRender: Component<{
     const wsRegions = props.wavesurferRef.getActivePlugins()[1] as RegionPlugin;
     const regions = wsRegions.getRegions();
 
-    await ffmpeg.load();
+    if (!ffmpeg.isLoaded()) {
+      await ffmpeg.load();
+    }
     ffmpeg.FS("writeFile", "video.mp4", await fetchFile(props.video));
 
     const regionCmd = regionToCommand(regions);
@@ -69,25 +71,69 @@ const VideoRender: Component<{
     <>
       <Show when={message()}>
         <p class="font-medium text-slate-700 mt-4 h-32 m-2">{message()}</p>
-        <Show
-          when={download()}
-          fallback={
-            <p class="text-center text-slate-700 font-bold">
-              {Math.floor(progress())}%
-            </p>
-          }
-        >
-          <a class="text-center text-slate-700 font-bold" href={download()}>
-            Righ click to save the video
-          </a>
-        </Show>
+        <p class="text-center text-slate-700 font-bold">
+          {Math.floor(progress())}%
+        </p>
       </Show>
       <button
         class="btn bg-lime-100 hover:bg-lime-500 hover:text-white m-2 "
         onClick={work}
       >
-        Render video
+        Export
       </button>
+      <Show when={download()}>
+        <div
+          class="ransition-opacity relative z-10"
+          aria-labelledby="modal-title"
+          role="dialog"
+          aria-modal="true"
+        >
+          <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
+
+          <div class="fixed inset-0 z-10 overflow-y-auto">
+            <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+              <div class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+                <div class="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+                  <div class="sm:flex sm:items-start">
+                    <div class="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+                      <h3
+                        class="text-base font-semibold leading-6 text-gray-900"
+                        id="modal-title"
+                      >
+                        Your video is ready!
+                      </h3>
+                      <div class="mt-2">
+                        <p class="text-sm text-gray-500">
+                          {
+                            "Refresh the browser to select a new video or close this message to make additional changes"
+                          }
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                  <a
+                    type="button"
+                    class="transition-opacity delay-100 inline-flex w-full justify-center rounded-md bg-lime-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-lime-600 sm:ml-3 sm:w-auto"
+                    href={download()}
+                    download
+                  >
+                    Download
+                  </a>
+                  <button
+                    type="button"
+                    class="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
+                    onClick={() => setDownload("")}
+                  >
+                    Close
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Show>
     </>
   );
 };
